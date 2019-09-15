57191e379b572c6ce324a2925e85d96b8478af17
wdg100 <wdg100@163.com>
2019-09-15 21:35:39 +0800

    fix the mysql_1.0.3.sql

--- libs/templates/sql/mysql_1.0.3.sql
+++ libs/templates/sql/mysql_1.0.3.sql
@@ -1,5 +1,5 @@
 
 
-ALTER TABLE `$handle_info` ADD COLUMN `cron` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL after `last_updated`
+ALTER TABLE `$handle_info` ADD COLUMN `cron` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL after `last_updated`;
 
-ALTER TABLE `$handle_info` CHANGE COLUMN `js` `js` text CHARACTER SET utf8mb4 DEFAULT NULL after `param`
+ALTER TABLE `$handle_info` CHANGE COLUMN `js` `js` text CHARACTER SET utf8mb4 DEFAULT NULL after `param`;
